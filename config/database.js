77e416a927bbb4b77d6ae88d77e2affc53c6959f
Juchan Kim <yj1877@berkeley.edu>
2017-11-06 16:43:34 -0800

    change database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 // config/database.js
 
 module.exports = {
-	url : 'mongodb://localhost/test'
+	// url : 'mongodb://localhost/test'
+	url : 'mongodb://yj1877:!qlqjs0214@skycast-shard-00-00-bmbis.mongodb.net:27017,skycast-shard-00-01-bmbis.mongodb.net:27017,skycast-shard-00-02-bmbis.mongodb.net:27017/test?ssl=true&replicaSet=skycast-shard-0&authSource=admin'
 }
